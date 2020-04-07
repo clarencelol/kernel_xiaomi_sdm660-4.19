b6f26a7feea12de8f2c385e5c4b9101f8f868879
Jules Irenge <jbi.octave@gmail.com>
2020-04-06 20:08:06 -0700

    mm/compaction: add missing annotation for compact_lock_irqsave
    Sparse reports a warning at compact_lock_irqsave()
    
    warning: context imbalance in compact_lock_irqsave() - wrong count at exit
    
    The root cause is the missing annotation at compact_lock_irqsave()
    Add the missing __acquires(lock) annotation.
    
    Signed-off-by: Jules Irenge <jbi.octave@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Link: http://lkml.kernel.org/r/20200214204741.94112-6-jbi.octave@gmail.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -350,6 +350,7 @@ static inline void update_pageblock_skip(struct compact_control *cc,
  */
 static bool compact_trylock_irqsave(spinlock_t *lock, unsigned long *flags,
 						struct compact_control *cc)
+	__acquires(lock)
 {
 	if (cc->mode == MIGRATE_ASYNC) {
 		if (!spin_trylock_irqsave(lock, *flags)) {
