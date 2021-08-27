823da6eef135e3eb28594676c4dad1c0988d7407
Reinazhard <reinazhard@gmail.com>
2021-08-28 00:21:24 +0700

    Revert "mm: ignore the boosting of watermark under lowmemory"
    This reverts commit feeb6c80da606ffc2eb73ca2e97b563cb4e3b4b1.
    
    Signed-off-by: Reinazhard <reinazhard@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2224,32 +2224,6 @@ static bool can_steal_fallback(unsigned int order, int start_mt)
 	return false;
 }
 
-static bool boost_eligible(struct zone *z)
-{
-	unsigned long high_wmark, threshold;
-	unsigned long reclaim_eligible, free_pages;
-
-	high_wmark = z->_watermark[WMARK_HIGH];
-	reclaim_eligible = zone_page_state_snapshot(z, NR_ZONE_INACTIVE_FILE) +
-			zone_page_state_snapshot(z, NR_ZONE_ACTIVE_FILE);
-	free_pages = zone_page_state(z, NR_FREE_PAGES);
-	threshold = high_wmark + (2 * mult_frac(high_wmark,
-					watermark_boost_factor, 10000));
-
-	/*
-	 * Don't boost watermark If we are already low on memory where the
-	 * boosting can simply put the watermarks at higher levels for a
-	 * longer duration of time and thus the other users relied on the
-	 * watermarks are forced to choose unintended decissions. If memory
-	 * is so low, kswapd in normal mode should help.
-	 */
-
-	if (reclaim_eligible < threshold && free_pages < threshold)
-		return false;
-
-	return true;
-}
-
 /*
  * This function implements actual steal behaviour. If order is large enough,
  * we can steal whole pageblock. If not, we first move freepages in this
